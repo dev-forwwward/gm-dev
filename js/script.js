efb757a44364309bafc418fd3952efd901fee256
beatriz-araujo <beatriz@forwwward.studio>
2025-07-10 18:08:37 +0100

    Version update
    Transition into rotating circle; structure remake

--- js/script.js
+++ js/script.js
@@ -4,24 +4,61 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const rotatorSection = document.querySelector('.rotator_section');
 
+    // note: zooms AND turns a bit
     gsap.timeline({
         scrollTrigger: {
-            trigger: rotatorSection,
+            trigger: '.zoom-left-trigger',
+            start: 'bottom 95%',
+            end: 'bottom top',
+            scrub: true,
+            markers: {
+                startColor: "orange",
+                endColor: "blue",
+            },
+        },
+    })
+        .to('.circle-section', {
+            css: {
+                left: '-80vw',
+                width: '100vw',
+                height: '100vw',
+            },
+            duration: 1
+        })
+        .to('.circle-section .slice-line-divider', {
+            opacity: 1,
+            duration: .5,
+            ease: 'power2.out'
+        }, "<")
+        .fromTo('.circle-section', {
+            rotate: "45deg",
+        }, {
+            delay: .1,
+            rotate: "15deg",
+            duration: 1
+        }, "<");
+
+
+    // fuller turn after initial small turn with zoom
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: '.rotation_trigger',
             start: "top top",
             end: "+=500%",
-            pin: true,
+            markers: {
+                startColor: "green",
+                endColor: "red",
+            },
             scrub: true,
-            // onEnter: ()=> {
-            //     gsap.to()
-            // }
+            pin: true
         }
     })
-        .fromTo('.circle-section-container', {
-            rotate: "30deg",
-        }, {
-            rotate: "-180deg",
-            duration: 1
-        });
+    .fromTo('.circle-section', {
+        rotate: "15deg",
+    }, {
+        rotate: "-168deg",
+        // duration: 1
+    });
     // .to('.circle-list-el-content', {
     //     delay: 1,
     //     opacity: 1,
