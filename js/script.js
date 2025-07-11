d217ea908370ba76c9fc9dd13810421ff4e1badc
beatriz-araujo <beatriz@forwwward.studio>
2025-07-11 19:02:17 +0100

    Update on circle slices behavior, added following section, added brand boxes (wip)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 gsap.registerPlugin(ScrollTrigger);
 
-document.addEventListener("DOMContentLoaded", () => {
+if (document.readyState === 'complete') {
+    init(); // page is already fully loaded
+} else {
+    window.addEventListener('load', init); // wait for it
+}
+
+function init() {
+
+    console.log("doc loaded");
 
     const rotatorSection = document.querySelector('.rotator_section');
 
@@ -15,15 +23,20 @@ document.addEventListener("DOMContentLoaded", () => {
                 startColor: "orange",
                 endColor: "blue",
             },
+            toggleActions: 'play none none reverse',
         },
     })
-        .to('.circle-section', {
-            css: {
-                left: '-80vw',
-                width: '100vw',
-                height: '100vw',
-            },
-            duration: 1
+        .fromTo('.circle-section', {
+            left: '0vw',
+            width: '50vw',
+            height: '50vw',
+            rotate: '45deg',
+        }, {
+            left: '-80vw',
+            width: '100vw',
+            height: '100vw',
+            rotate: '15deg',
+            duration: 1,
         })
         .to('.circle-section .slice-line-divider', {
             opacity: 1,
@@ -39,6 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }, "<");
 
 
+    // CIRCLE INNER SECTIONS ROTATION
     // fuller turn after initial small turn with zoom
     gsap.timeline({
         scrollTrigger: {
@@ -53,12 +67,10 @@ document.addEventListener("DOMContentLoaded", () => {
             pin: true
         }
     })
-    .fromTo('.circle-section', {
-        rotate: "15deg",
-    }, {
-        rotate: "-168deg",
-        // duration: 1
-    });
+        .to('.circle-section', {
+            rotate: "-168deg",
+            // duration: 1
+        });
     // .to('.circle-list-el-content', {
     //     delay: 1,
     //     opacity: 1,
@@ -66,25 +78,74 @@ document.addEventListener("DOMContentLoaded", () => {
     //     stagger: .5
     // }, "<");
 
-    document.querySelectorAll('.circle-list-container-outer').forEach((listEl) => {
-        let content = listEl.querySelector('.circle-list-el-content');
-        // ScrollTrigger.create({
-        //     trigger: listEl,
-        //     start: "top center",
-        //     end: "+=80%",
-        //     markers: true,
-        // })
-        gsap.to(content, {
-            scrollTrigger: {
-                trigger: listEl,
-                start: "top center",
-                end: "+=50%",
-                markers: true,
-            },
-            opacity: 1,
-            duration: .5,
-            ease: "ease2.out"
-        });
-    })
+    // document.querySelectorAll('.circle-list-container-outer').forEach((listEl) => {
+    //     let content = listEl.querySelector('.circle-list-el-content');
+    //     // ScrollTrigger.create({
+    //     //     trigger: listEl,
+    //     //     start: "top center",
+    //     //     end: "+=80%",
+    //     //     markers: true,
+    //     // })
+    //     gsap.to(content, {
+    //         scrollTrigger: {
+    //             trigger: listEl,
+    //             start: "top center",
+    //             end: "+=50%",
+    //             markers: true,
+    //         },
+    //         opacity: 1,
+    //         duration: .5,
+    //         ease: "ease2.out"
+    //     });
+    // });
 
-});
+
+
+    // BRAND CAROUSEL
+    // move circle up
+    gsap.fromTo('.circle-section', {
+        left: '-80vw',
+        top: '0vw',
+    },
+        {
+            scrollTrigger: {
+                trigger: '.brand_carousel_trigger',
+                start: 'top bottom',
+                end: '+=90%',
+                markers: {
+                    startColor: 'pink',
+                    endColor: 'pink'
+                },
+                scrub: true,
+                toggleActions: 'play none none reverse',
+            },
+            top: '-65vw',
+            left: '-42vw',
+            duration: .8,
+            ease: 'power2.inOut'
+        });
+
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: '.brand_carousel_trigger',
+            start: 'top top',
+            end: '+=200%',
+            scrub: true,
+            pin: true,
+            toggleActions: 'play none none reverse',
+        },
+    }).to('.slice-line-divider', {
+        rotateZ: '-90deg',
+        stagger: {
+            each: 0.015,
+            from: 9, // start from the 6th element (index-based)
+        },
+        ease: 'power2.inOut'
+    })
+    // .to(".circle-list-el-container:not(.has-box) .slice-line-divider", {
+    //     skewY: '-90deg',
+    //     ease: 'power2.inOut'
+    // }, "<");
+
+    ScrollTrigger.refresh();
+}
